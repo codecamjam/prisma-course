39185bb0fbc584330f117c36d70ed179e9de388c
codecamjam <camerontaghlabi@gmail.com>
2023-04-07 20:57:45 -0400

    l-10-11: more mw and validate request parameters

--- section3/src/server.ts
+++ section3/src/server.ts
@@ -1,17 +1,27 @@
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
+import morgan from 'morgan';
+// @ts-ignore
+import errorhandler from 'error-handler';
+import { body, validationResult } from 'express-validator';
 
 const app = express();
 const prisma = new PrismaClient();
 
 // custom middleware
-app.use((req, res, next) => {
-	console.log(`${req.url} ${new Date()}`);
-	next(); // call the next middleware in the stack
-});
+// app.use((req, res, next) => {
+//     console.log(`${req.url} ${new Date()}`);
+//     next(); // call the next middleware in the stack
+// });
+
+// apply third party middleware
+app.use(morgan('dev'));
+app.use(express.json()); // parse the request body for me
+
 
 app.get('/', (req, res) => {
-	res.send('HELLO WROLD');
+	throw new Error('SOMETHING WENT WRONG');
+	res.send('HELLO WORLD');
 });
 
 // GET api/course
@@ -33,37 +43,36 @@ app.get('/api/courses', async (req, res) => {
 // POST /api/course
 
 // create endpoint
-app.post('/api/course', async (req, res) => {
+app.post('/api/course',
+	body('title').isString().notEmpty(),
+	body('duration').isFloat().notEmpty(),
+	body('desc').isString().notEmpty(),
+	body('instructorId').isInt().optional(),
+	async (req, res) => {
 
-
-	// create a new instructor 
-	// const instructor = await prisma.instructor.create({
-	//     data:{
-	//         name: 'Jhony',
-	//         city: 'London',
-	//         country: 'UK',
-	//         email:'john@gmail.com',
-	//         zip: '2300'
-	//     }
-	// });
-	// create new record
-	const course = await prisma.course.create({
-		data: {
-			title: 'Learn Loopback',
-			desc: 'Build apis with Loopback',
-			duration: 12.4,
-			instructorId: 1
-			// Instructor:{
-			//     connect:{
-			//         id: 1
-			//     }
-			// }
+		// validate our parameters
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
 		}
+
+		const course = await prisma.course.create({
+			data: {
+				title: req.body.title,
+				desc: req.body.desc,
+				duration: req.body.duration,
+				instructorId: req.body.instructorId
+			}
+		});
+		return res.status(201).json(course);
 	});
 
-	// send the record back to response
-	res.status(201);
-	return res.json(course);
-});
+// app.use((error : any, req: express.Request,res: express.Response,next: express.NextFunction) => {
+//     console.log(`ERROR: ${error.message}`);
+//     next(error);
+// })
+if (process.env.NODE_ENV === 'development') {
+	app.use(errorhandler());
+}
 
 export default app;
